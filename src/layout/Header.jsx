0977bb2a33cd28c9ec641c2b4d3f466ff1010347
Daniel Dogeanu <contact@danieldogeanu.com>
2023-12-30 04:23:04 +0200

    Added SVG Logo to Header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,13 @@
 import { IconHelpCircle, IconMoonStars } from '@tabler/icons-react';
 import IconButton from '../components/IconButton';
+import ScaleLogo from '../assets/scale-logo.svg?react';
 import classes from '../styles/header.module.scss';
 
 export default function Header() {
   return (
     <header className={classes.header}>
       <div className={classes.left}>
-        Left
+        <ScaleLogo className={classes.logo} />
       </div>
       <div className={classes.center}>
         Center
