7ed470adc7ff3baf8ee21f2c6d0a3c1c1dfb621f
Daniel Dogeanu <contact@danieldogeanu.com>
2023-12-30 06:08:31 +0200

    Moved Logo to SiteLogo Component

---
+++ src/components/SiteLogo.jsx
@@ -0,0 +1,6 @@
+import ScaleLogo from '../assets/scale-logo.svg?react';
+import classes from '../styles/siteLogo.module.scss';
+
+export default function SiteLogo() {
+  return <ScaleLogo className={classes.siteLogo} />;
+}
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,13 @@
+import SiteLogo from '../components/SiteLogo';
 import FilterBar from '../components/FilterBar';
 import ActionMenu from '../components/ActionMenu';
-import ScaleLogo from '../assets/scale-logo.svg?react';
 import classes from '../styles/header.module.scss';
 
 export default function Header() {
   return (
     <header className={classes.header}>
       <div className={classes.left}>
-        <ScaleLogo className={classes.logo} />
+        <SiteLogo />
       </div>
       <div className={classes.center}>
         <FilterBar />
