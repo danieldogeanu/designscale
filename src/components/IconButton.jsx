88e6278eacdb55ebe51020656f385922198d49c6
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-03 09:20:25 +0200

    Added Dark Theme Colors

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,10 +1,12 @@
-import { ActionIcon } from '@mantine/core';
+import { ActionIcon, useMantineColorScheme } from '@mantine/core';
 
 /**
  * @param {{icon: JSX.Element, label: string}} props The element props.
  */
 export default function IconButton({icon, label, onClick}) {
+  const {colorScheme} = useMantineColorScheme();
   const TablerIcon = icon;
+
   return (
     <ActionIcon
       variant='outline'
@@ -13,7 +15,12 @@ export default function IconButton({icon, label, onClick}) {
       radius='md'
       aria-label={label}
       title={label}
-      onClick={onClick}>
+      onClick={onClick}
+      style={(theme) => ({
+        border: `calc(0.0625rem * 1) solid ${
+          colorScheme === 'light' ? theme.colors.gray[4] : theme.colors.gray[7]
+        }`
+      })}>
       <TablerIcon style={{width: '70%', height: '70%'}} stroke={1.5} />
     </ActionIcon>
   );
