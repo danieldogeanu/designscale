cd596232bcf1a5de217b1a4845c933fb66756ed4
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-05 14:10:00 +0200

    Added KeyUp Event Listener for Theme Switcher

--- src/components/ActionMenu.jsx
+++ src/components/ActionMenu.jsx
@@ -1,11 +1,16 @@
 import { Box, useMantineColorScheme } from '@mantine/core';
 import { IconHelpCircle, IconMoonStars, IconSun } from '@tabler/icons-react';
+import { useWindowEvent } from '@mantine/hooks';
 import IconButton from '../components/IconButton';
 import classes from '../styles/actionMenu.module.scss';
 
 export default function ActionMenu() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const isLightTheme = colorScheme === 'light' || colorScheme === 'auto';
+
+  useWindowEvent('keyup', (e) => {
+    if (e.key === 't') toggleColorScheme();
+  });
 
   return (
     <Box className={classes.actionMenu}>
