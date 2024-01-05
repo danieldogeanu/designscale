b5581be6ba240fca1183d647ea8bfbabac4aa8d9
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-05 13:27:21 +0200

    Made ScrollTop Responsive

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,21 +1,28 @@
 import { Affix, Transition } from '@mantine/core';
-import { useWindowScroll } from '@mantine/hooks';
+import { useWindowScroll, useMediaQuery } from '@mantine/hooks';
 import { IconArrowUp } from '@tabler/icons-react';
 import IconButton from './IconButton';
 
 export default function ScrollTop() {
   const [scroll, scrollTo] = useWindowScroll();
+  const matchesMediaQuery = useMediaQuery(`(min-width: 480px)`);
+  const buttonMargin = matchesMediaQuery ? 40 : 28;
+  const buttonSize = matchesMediaQuery ? 40 : 48;
 
   return (
-    <Affix position={{bottom: 40, right: 40}}>
+    <Affix position={{bottom: buttonMargin, right: buttonMargin}}>
       <Transition transition='slide-up' mounted={scroll.y > 0}>
         {(transitionStyles) => (
           <IconButton 
             icon={IconArrowUp} 
             label='Scroll to Top' 
-            style={transitionStyles} 
             onClick={() => scrollTo({y: 0})}
-            size={40}
+            size={buttonSize}
+            style={(theme) => ({
+              backgroundColor: theme.white,
+              boxShadow: theme.shadows.sm,
+              ...transitionStyles,
+            })} 
           />
         )}
       </Transition>
