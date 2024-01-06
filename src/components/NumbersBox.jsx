e34e17f18fd9af9dfddc72b1dc06237f0ccd26a4
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-06 15:03:46 +0200

    Added Empty State for NumbersBox

--- src/components/NumbersBox.jsx
+++ src/components/NumbersBox.jsx
@@ -7,14 +7,16 @@ import classes from '../styles/numbersBox.module.scss';
 export default function NumbersBox() {
   const {state} = useFilter();
   const numbers = useNumbersScale(state.size, state.scale);
+  const filteredNumbers = numbers.filter((value) => {
+    return state.filter !== null ? value.toString().includes(state.filter) : true;
+  }).map((number, index) => {
+    return <NumberCell key={index}>{number}</NumberCell>;
+  });
+  const NumberNotFound = <Box className={classes.notFound}>Number not found.</Box>;
 
   return (
     <Box className={classes.numbersBox}>
-      {numbers && numbers.filter((value) => {
-        return state.filter !== null ? value.toString().includes(state.filter) : true;
-      }).map((number, index) => {
-        return <NumberCell key={index}>{number}</NumberCell>;
-      })}
+      {(filteredNumbers && filteredNumbers.length > 0) ? filteredNumbers : NumberNotFound}
     </Box>
   );
 }
