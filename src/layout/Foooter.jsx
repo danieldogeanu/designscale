598db02be73d8775aa31cb289cd0de94c47107e7
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-06 14:47:07 +0200

    Changed Footer Layout

--- src/layout/Foooter.jsx
+++ src/layout/Foooter.jsx
@@ -1,10 +1,12 @@
 import { IconBrandGithub, IconBrandLinkedin, IconBrandInstagram, IconBrandDribbble } from '@tabler/icons-react';
 import { Group, Stack, TypographyStylesProvider } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import SocialIcon from '../components/SocialIcon';
-import classes from '../styles/footer.module.scss';
 import CoffeeButton from '../components/CoffeeButton';
+import classes from '../styles/footer.module.scss';
 
 export default function Footer() {
+  const matchesMediaQuery = useMediaQuery('(min-width: 480px)');
   const urls = {
     danieldogeanu: 'https://danieldogeanu.com',
     project: 'https://github.com/danieldogeanu/scale-cheat-sheet',
@@ -17,23 +19,25 @@ export default function Footer() {
   return (
     <TypographyStylesProvider style={{paddingLeft: 0}}>
       <footer className={classes.footer}>
+        <div className={classes.footerContainer}>
+          <div className={classes.socials}>
+            <Group gap={4} justify={matchesMediaQuery ? 'flex-end' : 'center'}>
+              <SocialIcon label='LinkedIn' icon={IconBrandLinkedin} href={urls.linkedin} />
+              <SocialIcon label='GitHub' icon={IconBrandGithub} href={urls.github} />
+              <SocialIcon label='Dribbble' icon={IconBrandDribbble} href={urls.dribbble} />
+              <SocialIcon label='Instagram' icon={IconBrandInstagram} href={urls.instagram} />
+            </Group>
+          </div>
+          <div className={classes.copyright}>
+            <Stack gap={0}>
+              <p>Made by <a href={urls.danieldogeanu} target='_blank' rel='noreferrer'>Daniel Dogeanu</a>.</p>
+              <p>View project on <a href={urls.project} target='_blank' rel='noreferrer'>GitHub</a>.</p>
+            </Stack>
+          </div>
+        </div>
         <div className={classes.donate}>
           <p>If this app was useful to you...</p>
           <CoffeeButton />
-        </div>
-        <div className={classes.socials}>
-          <Group gap={4} justify='center'>
-            <SocialIcon label='LinkedIn' icon={IconBrandLinkedin} href={urls.linkedin} />
-            <SocialIcon label='GitHub' icon={IconBrandGithub} href={urls.github} />
-            <SocialIcon label='Dribbble' icon={IconBrandDribbble} href={urls.dribbble} />
-            <SocialIcon label='Instagram' icon={IconBrandInstagram} href={urls.instagram} />
-          </Group>
-        </div>
-        <div className={classes.copyright}>
-          <Stack gap={0}>
-            <p>Made by <a href={urls.danieldogeanu} target='_blank' rel='noreferrer'>Daniel Dogeanu</a>.</p>
-            <p>View project on <a href={urls.project} target='_blank' rel='noreferrer'>GitHub</a>.</p>
-          </Stack>
         </div>
       </footer>
     </TypographyStylesProvider>
