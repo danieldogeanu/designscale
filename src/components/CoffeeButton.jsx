55272b14185a28c4aa7b547a9fe5a1899085d0ff
Daniel Dogeanu <contact@danieldogeanu.com>
2024-01-06 15:45:33 +0200

    Refactored CoffeeButon

--- src/components/CoffeeButton.jsx
+++ src/components/CoffeeButton.jsx
@@ -1,24 +1,12 @@
 import classes from '../styles/coffeeButton.module.scss';
+import CoffeeLogo from '../assets/bmc-logo.svg?react';
 
 export default function CoffeeButton() {
   return (
     <div className={classes.coffeeButton}>
       <a href="https://www.buymeacoffee.com/danieldogeanu" target="_blank" rel="noreferrer">
-        <img src="https://cdn.buymeacoffee.com/buttons/v2/arial-yellow.png" alt="Buy Me A Coffee" />
+        <CoffeeLogo className={classes.coffeeLogo} /> Buy Me a Coffee
       </a>
-      <script 
-        type="text/javascript" 
-        src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" 
-        data-name="bmc-button" 
-        data-slug="danieldogeanu" 
-        data-color="#FFDD00" 
-        data-emoji="" 
-        data-font="Inter" 
-        data-text="Buy Me a Coffee" 
-        data-outline-color="#000000" 
-        data-font-color="#000000" 
-        data-coffee-color="#ffffff"
-      ></script>
     </div>
   );
 }
